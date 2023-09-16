d7a0519c8cdf81018c8dccd53d3af978d30141ed
LeRoy Rosen <113536685+NordiskPirat@users.noreply.github.com>
2023-09-16 14:15:42 -0600

    finishing touches

--- wdd230/scripts/getdates.js
+++ wdd230/scripts/getdates.js
@@ -1,2 +1,6 @@
 const date = new Date(document.lastModified);
 document.getElementById("datemodified").innerHTML = date;
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentYear").innerHTML = year;
