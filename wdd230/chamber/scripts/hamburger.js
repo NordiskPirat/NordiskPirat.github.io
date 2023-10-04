5847982da62b8f064750baecf8435912054d58cd
LeRoy Rosen <113536685+NordiskPirat@users.noreply.github.com>
2023-10-04 01:03:22 -0600

    chamber

---
+++ wdd230/chamber/scripts/hamburger.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector("#menu");
+const navi = document.querySelector(".navi");
+
+hamburger.addEventListener("click", () => {
+  navi.classList.toggle("open");
+  hamburger.classList.toggle("open");
+});
