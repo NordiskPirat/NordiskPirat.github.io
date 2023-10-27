f20129606b0eefdad7518979a800e113289fa704
LeRoy Rosen <113536685+NordiskPirat@users.noreply.github.com>
2023-10-27 17:21:41 -0600

    1

--- wdd230/lesson07/scripts/scripture.js
+++ wdd230/lesson07/scripts/scripture.js
@@ -1,20 +1,22 @@
 const input = document.querySelector("#favoritechapt");
-
 const button = document.querySelector("button");
-
 const list = document.querySelector("#list");
+
+let chaptersArray = getChapterList() || [];
 
 button.addEventListener("click", () => {
   if (input.value == "") {
     alert("Cannot be blank.");
     input.focus();
   } else input.focus();
+
   const li = document.createElement("li");
   const deletebutton = document.createElement("button");
   li.textContent = input.value;
   deletebutton.textContent = "❌";
   li.append(deletebutton);
   list.append(li);
+
   deletebutton.addEventListener("click", () => {
     list.removeChild(list.firstChild);
     input.focus();
