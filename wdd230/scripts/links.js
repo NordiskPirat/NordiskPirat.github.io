93943206548ade56d1994915669aa73a7a94f0fa
LeRoy Rosen <113536685+NordiskPirat@users.noreply.github.com>
2023-11-09 18:13:25 -0700

    1

--- wdd230/scripts/links.js
+++ wdd230/scripts/links.js
@@ -1,26 +1,24 @@
-const baseURL = "https://nordiskpirat.github.io/wdd230";
-const linksurl = "data/links.json";
-const linkMenu = document.querySelector("#courseLinks");
+const baseURL = "https://NordiskPirat.github.io/wdd230/";
+const linksURL = "data/links.json";
+const menu = document.querySelector("#courseLinks");
 
-async function getLinkData() {
-  const response = await fetch(linksurl);
+async function getLinks() {
+  const response = await fetch(linksURL);
   const data = await response.json();
   displayLinks(data.lessons);
 }
-
 const displayLinks = (weeks) => {
   weeks.forEach((week) => {
     let assignment = document.createElement("li");
     assignment.textContent = `Week ${week.lesson}: `;
     week.links.forEach((link) => {
       let individual = document.createElement("a");
-      individual.setAttribute("href", `${link.url}`);
-      individual.setAttribute("target", "_blank");
       individual.textContent = `${link.title} `;
+      individual.setAttribute("target", "_blank");
+      individual.setAttribute("href", `${link.url}`);
       assignment.appendChild(individual);
     });
-    linkMenu.appendChild(assignment);
+    menu.appendChild(assignment);
   });
 };
-
-getLinkData();
+getLinks();
